6444f1b6f5825c7f750c54eaa10deef820284440
Brandon Toups <bmt0015@auburn.edu>
2018-03-26 13:02:01 -0500

    Fix commenting and add example

--- between.rkt
+++ between.rkt
@@ -1,12 +1,17 @@
 #lang racket
 ; Brandon Toups
 ; bmt0015@auburn.edu
-; COMP-3220-001
-; 12-10-2017
+; 10 December 2017
 
-; Problem Description: Define a Scheme function named
-; between so that (between I J K) returns true iff
-; K is an integer inclusively between I and J. 
+; A Scheme function named "between" such that 
+; (between I J K) returns true iff K is an 
+; integer inclusively between I and J. 
+; I <= K <= J
+
+; Sample input: 
+; (between 5 10 7)    => #t
+; (between 5 10 11)   => #f
+
 (define (between I J K)
   (if (and (<= I K) (<= K J)) 
     #t
